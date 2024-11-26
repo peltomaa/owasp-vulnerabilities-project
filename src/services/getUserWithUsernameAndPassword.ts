145be71706a2be23ebc09ae3a6c795f2b78c0276
Touko Peltomaa <touko.peltomaa@gmail.com>
2024-11-27 00:04:37 +0200

    Add bcrypt

--- src/services/getUserWithUsernameAndPassword.ts
+++ src/services/getUserWithUsernameAndPassword.ts
@@ -1,19 +1,27 @@
 import { db } from "../db/client";
 import { User } from "../types/User";
+import { comparePasswords } from "../utils/comparePasswords";
 
 export const getUserWithUsernameAndPassword = async (
   username: string,
   password: string,
 ) => {
-  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+  const query = `SELECT * FROM users WHERE username = '${username}'`;
 
   return new Promise<User | undefined>((resolve, reject) => {
-    db.get<User>(query, (err, res) => {
+    db.get<User>(query, async (err, res) => {
       if (err) {
         return reject(err);
       }
 
       if (!res) {
+        return resolve(undefined);
+      }
+
+      console.log(password, res);
+      const isMatch = await comparePasswords(password, res.password);
+
+      if (!isMatch) {
         return resolve(undefined);
       }
 
